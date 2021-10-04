a76f82f6d7246556d6a18f1c908158479c1d9f82
tkhattar <tkhattar@student.42.fr>
2021-10-04 07:07:01 -0700

    si

--- src/color.c
+++ src/color.c
@@ -73,7 +73,7 @@ int	get_color(int z, t_map map)
 	color[9] = assign_point(0xAA3E0B, 0xF2F5FE);
 	i = floor(find_perc(map.min_z, map.max_z, z) * 5);
 	//lmin = map.min_z + (i * 0.200) * (map.rng);
-	lmax = (i == 5) ? map.max_z : map.min_z + (0.2 + i * 0.200) * (map.rng);
+	//lmax = (i == 5) ? map.max_z : map.min_z + (0.2 + i * 0.200) * (map.rng);
 	lmax = assign_if ( i == 5, map.max_z, map.min_z + (0.2 + i * 0.200) * (map.rng))
 	(i == 5) ? i = 4 : 0;
 	rgb = interp_color(color[i + map.cm % 10].x, color[i + map.cm % 10].y,
